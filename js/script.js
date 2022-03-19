d64d6889b065d835e53e2685683897943baded7f
ZippMukami <zimukami@gmail.com>
2022-03-20 02:55:48 +0300

    defines constants and adds event listeners.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-
 let carts = document.querySelectorAll(".add-cart");
+
+for (let i = 0; i < carts.length; i + 1) {
+    carts[i].addEventListener(click, function () {
+        cartNumber(products[i]);
+        totalCost(products[i]);
+    })
+};
+
+
+function cartNumber(product) {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+
+   
+};
