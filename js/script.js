f0e9d051cf37972437369f5c9de30cba77f7eb21
ZippMukami <zimukami@gmail.com>
2022-03-20 14:14:19 +0300

    creates a cartNumber function.

--- js/script.js
+++ js/script.js
@@ -6,14 +6,69 @@ let products= [
         tag    : "small pizza",
         price  : 500,
         inCart : 0
-    }
+    },
+
+    {
+        name   : "medium",
+        tag    : "medium pizza",
+        price  : 1000,
+        inCart : 0
+    },
+
+    {
+        name   : "large",
+        tag    : "large pizza",
+        price  : 1500,
+        inCart : 0
+    },
+
+    {
+        name   : "cheese",
+        tag    : "cheese toppings",
+        price  : 200,
+        inCart : 0
+    },
+
+    {
+        name   : "olive",
+        tag    : "olive toppings",
+        price  : 200,
+        inCart : 0
+    },
+
+    {
+        name   : "pepperoni",
+        tag    : "pepperoni toppings",
+        price  : 200,
+        inCart : 0
+    },
+
+    {
+        name   : "thin",
+        tag    : "thin Crust",
+        price  : 200,
+        inCart : 0
+    },
+
+    {
+        name   : "thick",
+        tag    : "thick Crust",
+        price  : 200,
+        inCart : 0
+    },
+
+    {
+        name   : "crispy",
+        tag    : "crispy Crust",
+        price  : 200,
+        inCart : 0
+    },
 ]
 
 for (let i = 0; i < carts.length; i++) {
     carts[i].addEventListener('click', function () {
-        console.log("cart");
          cartNumber(products[i]);
-         totalCost(products[i]);
+        //  totalCost(products[i]);
     })
 };
 
@@ -21,6 +76,7 @@ for (let i = 0; i < carts.length; i++) {
 function cartNumber(product) {
     let productNumbers = localStorage.getItem('cartNumbers');
     productNumbers = parseInt(productNumbers);
+    localStorage.setItem('cartNumbers')
 
 
 };
